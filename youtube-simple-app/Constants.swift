bcd5bc6480ba801f8a87abd4c1828dcc11716e2e
Burak Ekmen <ekmen.burak@hotmail.com>
2020-11-26 21:41:31 +0300

    youtube api istek model eklendi

---
+++ youtube-simple-app/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  youtube-simple-app
+//
+//  Created by BURAK EKMEN on 26.11.2020.
+//  Copyright © 2020 Burak Ekmen. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static var API_KEY = "AIzaSyCO0i9d9f_nfPOZhsi6QHAR7-ipPchlNMw"
+    static var PLAYLIST_ID = "RDCMUC2D6eRvCeMtcF5OGHf1-trw"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
---
+++ youtube-simple-app/Video.swift
@@ -0,0 +1,17 @@
+//
+//  Video.swift
+//  youtube-simple-app
+//
+//  Created by BURAK EKMEN on 26.11.2020.
+//  Copyright © 2020 Burak Ekmen. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
