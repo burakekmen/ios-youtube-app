295574579e1ad0d7dddfba4db1b8ff35061f9b3a
Burak Ekmen <ekmen.burak@hotmail.com>
2020-12-01 22:48:41 +0300

    tableview eklendi, delegate eklendi

--- youtube-simple-app/Constants.swift
+++ youtube-simple-app/Constants.swift
@@ -10,8 +10,9 @@ import Foundation
 
 
 struct Constants {
-    static var API_KEY = ""
-    static var PLAYLIST_ID = ""
+    static var API_KEY = "AIzaSyCO0i9d9f_nfPOZhsi6QHAR7-ipPchlNMw"
+    static var PLAYLIST_ID = "RDCMUC2D6eRvCeMtcF5OGHf1-trw"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static var VIDEOCELL_ID = "VideoCell"
 }
 
